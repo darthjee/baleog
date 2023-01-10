9cf1cb847484ed939d3f2b05a8fd83aa87232d11
darthjee <darthjee@gmail.com>
2023-01-10 12:39:56 -0300

    Add docs

--- lib/baleog/endpoint.rb
+++ lib/baleog/endpoint.rb
@@ -12,6 +12,20 @@ class Baleog
 
     HTTP_METHODS = %i[get post delete patch].freeze
 
+    # @method path
+    #
+    # Endpoint path where the request will be made
+    #
+    # @return [String] the endpoint path
+
+    # @method http_method
+    #
+    # Endpoint http method, or action, that will be used in the endpoint
+    #
+    # The available list is included in {HTTP_METHODS}
+    #
+    # @return [Symbol] the http method
+
     def initialize(path: nil, http_method: :get)
       @path        = path
       @http_method = http_method

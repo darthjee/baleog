c0eeb90cb0bd1eaf3a6784a3790ec355908f3fb4
darthjee <darthjee@gmail.com>
2023-05-03 18:05:22 -0300

    Add doc to Moder::Builder

--- lib/baleog/model/builder.rb
+++ lib/baleog/model/builder.rb
@@ -2,13 +2,29 @@
 
 module Baleog
   class Model
+    # @api private
+    # @author darthjee
+    #
+    # Builder responsible for adding methods to {Model}
     class Builder < Sinclair
+      # Add fields to the model
+      #
+      # @param field_name [Array<Symbol>] fields to be added
+      #
+      # @see #add_field
+      #
+      # @return [Array<Symbol>] added fields
       def add_fields(field_names)
         field_names.each do |field_name|
           add_field(field_name)
         end
       end
 
+      # Add field to the model
+      #
+      # This will add the +reader+ and +setter+
+      #
+      # @return [Array<Sinclair::MethodDefinition>]
       def add_field(field_name, key: field_name, cast: nil)
         name = key.to_s
         caster = ValueWrapper.caster_for(cast)

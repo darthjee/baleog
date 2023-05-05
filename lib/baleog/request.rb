ae19f6416ebdb27d4ffd05e1b3c8bea8bec434b4
darthjee <darthjee@gmail.com>
2023-05-05 14:09:09 -0300

    Rubocop fix

--- lib/baleog/request.rb
+++ lib/baleog/request.rb
@@ -7,10 +7,11 @@ module Baleog
     initialize_with(:endpoint, { headers: {}, payload: nil }, **{})
 
     def call
-      url = [base_url, path.gsub(/^\//,'')].join('/')
+      url = [base_url, path.gsub(%r{^/}, '')].join('/')
       response = Faraday.get(url)
 
       return JSON.parse(response.body) unless model
+
       model.from(response.body)
     end
 

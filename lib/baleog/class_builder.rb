560c0524b55211722925f40a1c81fdc959f6c822
darthjee <darthjee@gmail.com>
2023-05-08 19:26:52 -0300

    Add minimal documentation

--- lib/baleog/class_builder.rb
+++ lib/baleog/class_builder.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module Baleog
+  # @api private
+  # @author darthjee
+  #
+  # Module responsible for generating a new class nested
+  #
+  # The new class will be based on a class nested in the superclass
+  # under the same name.
+  #
+  # This is required to enable different configurations for different
+  # clients
   module ClassBuilder
     def self.build(parent_module, name)
       code = <<-RUBY

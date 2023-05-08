574e74f3e66966b4649060ddb4e469f08ba05cb9
darthjee <darthjee@gmail.com>
2023-05-08 08:30:34 -0300

    Use endpoint factory

--- spec/support/factories/baleog/endpoint.rb
+++ spec/support/factories/baleog/endpoint.rb
@@ -4,6 +4,21 @@ FactoryBot.define do
   factory :baleog_endpoint, class: 'Baleog::Endpoint' do
     skip_create
 
+    initialize_with do
+      endpoint_class.new(**endpoint_attributes)
+    end
+
+    endpoint_class { Baleog::Endpoint }
+
+  endpoint_attributes do
+      {
+        service: service,
+        path: path,
+        http_method: http_method,
+        model: model
+      }
+    end
+
     service     { create(:baleog_service) }
     path        { '/some_path' }
     http_method { :get }

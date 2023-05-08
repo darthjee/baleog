2b8c0e8469e172ea4a42208c1cd47c724bf173fc
darthjee <darthjee@gmail.com>
2023-05-08 19:21:54 -0300

    Revert "Refactor builder"
    This reverts commit 93e668d5dda72f91bd339a16207e21fd1aaa8706.

--- lib/baleog/class_builder.rb
+++ lib/baleog/class_builder.rb
@@ -1,33 +1,18 @@
 # frozen_string_literal: true
 
 module Baleog
-  class ClassBuilder < Sinclair::Model
-    initialize_with :parent_module, :name
-
+  class ClassBuilder
     def self.build(parent_module, name)
-      new(
-        parent_module: parent_module,
-        name: name
-      ).build
-    end
-
-    def build
-      block.call(parent_module)
-    end
-
-    private
-
-    def code
-      <<-RUBY
+      code = <<-RUBY
         proc do |parent_module|
           class parent_module::#{name} < parent_module.superclass::#{name}
           end
         end
       RUBY
-    end
 
-    def block
-      eval(code)
+      prc = eval(code)
+
+      prc.call(parent_module)
     end
   end
 end

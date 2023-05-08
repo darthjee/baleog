93e668d5dda72f91bd339a16207e21fd1aaa8706
darthjee <darthjee@gmail.com>
2023-05-08 19:20:09 -0300

    Refactor builder

--- lib/baleog/class_builder.rb
+++ lib/baleog/class_builder.rb
@@ -1,18 +1,33 @@
 # frozen_string_literal: true
 
 module Baleog
-  class ClassBuilder
+  class ClassBuilder < Sinclair::Model
+    initialize_with :parent_module, :name
+
     def self.build(parent_module, name)
-      code = <<-RUBY
+      new(
+        parent_module: parent_module,
+        name: name
+      ).build
+    end
+
+    def build
+      block.call(parent_module)
+    end
+
+    private
+
+    def code
+      <<-RUBY
         proc do |parent_module|
           class parent_module::#{name} < parent_module.superclass::#{name}
           end
         end
       RUBY
+    end
 
-      prc = eval(code)
-
-      prc.call(parent_module)
+    def block
+      eval(code)
     end
   end
 end

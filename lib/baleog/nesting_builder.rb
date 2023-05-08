1829c4018733f8bb46bb0ccd949e8aaf185a1272
darthjee <darthjee@gmail.com>
2023-05-08 19:59:50 -0300

    Add minimal documentation

--- lib/baleog/nesting_builder.rb
+++ lib/baleog/nesting_builder.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 module Baleog
+  # @api private
+  # @author darthjee
+  #
+  # Module exposing method to build nested classes under it
+  #
+  # This will ensure that the nested classes are configurable without
+  # affecting other clients
   module NestingBuilder
     autoload :ClassBuilder, 'baleog/nesting_builder/class_builder'
 

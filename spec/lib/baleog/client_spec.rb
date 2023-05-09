7df37de0bbb1201a5210492754f3f54153e303d5
darthjee <darthjee@gmail.com>
2023-05-09 06:03:45 -0300

    Add double inheritance spec

--- spec/lib/baleog/client_spec.rb
+++ spec/lib/baleog/client_spec.rb
@@ -3,7 +3,8 @@
 require 'spec_helper'
 
 describe Baleog::Client do
-  let(:client_class) { Class.new(described_class) }
+  let(:client_class) { Class.new(base_class) }
+  let(:base_class)   { described_class }
 
   describe 'inheritance' do
     it 'builds a nested model class' do
@@ -14,6 +15,20 @@ describe Baleog::Client do
     it 'builds a nested value wrapper class' do
       expect(client_class::Model::ValueWrapper.superclass)
         .to eq(described_class::Model::ValueWrapper)
+    end
+
+    context 'when base class is alread inheriting from client' do
+      let(:base_class) { Class.new(described_class) }
+
+      it 'builds a nested model class' do
+        expect(client_class::Model.superclass)
+          .to eq(base_class::Model)
+      end
+
+      it 'builds a nested value wrapper class' do
+        expect(client_class::Model::ValueWrapper.superclass)
+          .to eq(base_class::Model::ValueWrapper)
+      end
     end
   end
 end

0f0c726609a112ade4e7254055a36881a2a772e7
darthjee <darthjee@gmail.com>
2023-05-09 07:17:47 -0300

    Configure through class method

--- lib/baleog/adapter.rb
+++ lib/baleog/adapter.rb
@@ -9,10 +9,14 @@ module Baleog
       end
 
       def adapters
-        @adapters ||= {
-          faraday: 'baleog/adapter/faraday'
-        }
+        @adapters ||= {}
+      end
+
+      def with_adapter(name, file:)
+        adapters[name] = file
       end
     end
+
+    with_adapter(:faraday, file: 'baleog/adapter/faraday')
   end
 end

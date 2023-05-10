0a2e0d67e6431ff98e965334ba32dac29d0d09b5
darthjee <darthjee@gmail.com>
2023-05-10 12:05:32 -0300

    Shortcircuit return

--- lib/baleog/client/endpoint_builder.rb
+++ lib/baleog/client/endpoint_builder.rb
@@ -4,7 +4,9 @@ module Baleog
   class Client
     class EndpointBuilder < Sinclair
       def add_endpoint(method_name)
-        add_method(method_name) {} if method_name
+        return unless method_name
+
+        add_method(method_name) {}
       end
 
       def build
